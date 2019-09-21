11a50c0286b1e5b0ca65b61c689e8d90f15d3bdd
ChenLin12138 <scorpion_chenlin@163.com>
2019-09-21 21:01:08 +0800

    Project init

---
+++ src/main/java/com/lin/speicalroute/SpecialrouteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lin.speicalroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialrouteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecialrouteServiceApplication.class, args);
+	}
+
+}

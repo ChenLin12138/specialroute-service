a3fb38acd93addb280f5a3082d73c09a1ca6ce1d
ChenLin12138 <scorpion_chenlin@163.com>
2019-09-24 21:09:02 +0800

    Update Configration SQL

--- createTable.sql
+++ createTable.sql
@@ -6,4 +6,4 @@ CREATE TABLE `abtesting` (
   PRIMARY KEY (`service_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='abtesting';
 
-INSERT INTO abtesting (service_name, active,  endpoint, weight) VALUES ('organizationservice', 'Y','http://orgservice-new:8087',5);
+INSERT INTO abtesting (service_name, active,  endpoint, weight) VALUES ('organizationservice', 'Y','http://organizationservice-new:8082',5);
